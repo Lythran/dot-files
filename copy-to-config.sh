902f0c6ca3bd2e97f1c2cdba243794992feb2cd7
Lythran <me.9jtrt@slmail.me>
2025-06-25 12:57:23 -0400

    made the bash script

---
+++ copy-to-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Copying to ~/.config folder..."
+sleep 3 
+echo "Copying hyprland config..."
+cp -rf hypr ~/.config
+echo "Copying rofi config..."
+cp -rf rofi ~/.config
+echo "Copying waybar config..."
+cp -rf waybar ~/.config
+echo "Completed! be sure to check the config files over at ~/.config and tweak it as you wish"
