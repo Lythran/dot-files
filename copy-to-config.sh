3b41d647bc6141d66ef90e7cd4dcc7b3aa872c4b
Lythran <me.9jtrt@slmail.me>
2025-06-26 14:32:26 -0400

    added swappy to script

--- copy-to-config.sh
+++ copy-to-config.sh
@@ -7,4 +7,6 @@ echo "Copying rofi config..."
 cp -rf rofi ~/.config
 echo "Copying waybar config..."
 cp -rf waybar ~/.config
+echo "Copying swappy to config..."
+cp -rf swappy ~/.config
 echo "Completed! be sure to check the config files over at ~/.config and tweak it as you wish"
