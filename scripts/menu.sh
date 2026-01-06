e87ff153aad15f8973fa3955277c3cc90759fd0d
Lythran <me.9jtrt@slmail.me>
2026-01-06 11:37:55 -0500

    more sleek menu

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -14,7 +14,7 @@ power() {
 while true; do
 	choice=$(gum choose --header "What do you want to do?" "Update All" "Update Mirrors" "Web Apps" "Power Options" "Exit")
 	case $choice in
-		"Update All") yay -Syu ;;
+		"Update All") $HOME/scripts/update.sh ;;
 		"Power Options") power ;;
 		"Update Mirrors") sudo reflector --latest 200 --country "United States" --sort rate --protocol https --age 24 --download-timeout 10 --save /etc/pacman.d/mirrorlist ;;
 		"Web Apps") $HOME/scripts/pwa.sh;;
